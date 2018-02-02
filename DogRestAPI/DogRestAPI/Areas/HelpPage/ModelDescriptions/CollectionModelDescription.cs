93d19627ec79dd8c25055ba013bbd47007f2f531
matti <matti@LAPTOP-T1JAIQK0>
2018-02-02 13:34:57 +0100

    new setup

--- DogRestAPI/DogRestAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace DogRestAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
