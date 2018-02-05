40ca51d838745ac10a35e4a87f48e4e7c23e8036
matti <matti@LAPTOP-T1JAIQK0>
2018-02-05 15:30:41 +0100

    get and half post

--- Controllers/DogsController.cs
+++ Controllers/DogsController.cs
@@ -2,33 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
 
 namespace dogapi_Mattias_Lundh.Controllers
 {
-    [Route("api/{controller}")]
-    public class ValuesController : Controller
+    [Route("[controller]")]
+    public class DogsController : Controller
     {
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            Response.ContentType = "application/json";
+            return GetDogs().Select(d => d[1].Substring(d[1].IndexOf("breedName") + 12, d[1].Length - 3 - (d[1].Substring(0, d[1].IndexOf("breedName") + 12).Length)));
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<string> Get(string id)
         {
-            string result = Convert.ToString(System.IO.File.ReadAllLines(@"/DogFiles/" + id + ".json"));
-            return result;
+            Response.ContentType = "application/json";
+            return GetDog(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]FormCollection collection)
         {
+            if (collection["BreedName"] != "" && collection["WikipediaUrl"] != "" && collection["Description"] != "")
+            {
+                System.IO.File.Create(Environment.CurrentDirectory + @"/DogFiles/" + collection["BreedName"] + @".json");
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
         }
 
         // PUT api/values/5
@@ -42,5 +51,58 @@ namespace dogapi_Mattias_Lundh.Controllers
         public void Delete(int id)
         {
         }
+
+        private List<List<string>> GetDogs()
+        {
+            List<string> dogPaths = FindAllFilePaths(@"/DogFiles/");
+            List<List<string>> dogs = new List<List<string>>();
+            foreach (string path in dogPaths)
+            {
+                List<string> dog = new List<string>();
+                dog.AddRange(System.IO.File.ReadAllLines(path));
+                dogs.Add(dog);
+            }
+            return dogs;
+        }
+
+        private List<string> GetDog(string request)
+        {
+            List<string> dogPaths = FindAllFilePaths(@"/DogFiles/");
+            Dictionary<string, List<string>> dogs = new Dictionary<string, List<string>>();
+            foreach (string path in dogPaths)
+            {
+                List<string> dog = new List<string>();
+                dog.AddRange(System.IO.File.ReadAllLines(path));
+                int index = path.IndexOf(@"DogFiles/") + 9;
+                dogs.Add(path.Substring(index, path.Length - index - 5), dog);
+            }
+
+            if (dogs[request] != null)
+            {
+                return dogs[request];
+            }
+            throw new Exception("invalid dog, no recource found");
+        }
+
+        private List<string> FindAllFilePaths(string dir)
+        {
+            List<string> result = new List<string>();
+            foreach (string entity in System.IO.Directory.GetFileSystemEntries(Environment.CurrentDirectory + dir, "*"))
+            {
+                if (entity.Substring(entity.Length - 5, 5).Contains("."))
+                {
+                    result.Add(entity);
+                }
+                else
+                {
+                    foreach (string s in FindAllFilePaths(entity))
+                    {
+                        result.Add(s);
+                    }
+                }
+            }
+            return result;
+        }
+
     }
 }
